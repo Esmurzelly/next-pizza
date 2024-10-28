2b26637af82872d9689b5166cbeb1eb8c9450250
Dark Lord <adamsuper148@gmail.com>
2024-10-28 15:12:13 +0300

    topBard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <main className="min-h-screen">
+          <Header />
           {children}
         </main>
       </body>
---
+++ components/shared/index.ts
@@ -0,0 +1,5 @@
+export { Container } from './container'
+export { Title } from './title'
+export { Header } from './header'
+export { SortPopup } from './sort-popup'
+export { TopBar } from './top-bar'
